9f5c83cc24440639cf23ce5fed07b0fedc5ef26c
abhay <abhay94kushwah>
2022-04-07 15:26:56 +0530

    if and else advance

---
+++ src/conditional_statement/Operatorgreaterthanequalto.java
@@ -0,0 +1,29 @@
+package conditional_statement;
+
+public class Operatorgreaterthanequalto {
+    public static void main(String[] args){
+        int age=18;
+        if(age>=18){
+            System.out.println("you are elegible for voting");
+        }
+        else{
+            System.out.println("you are not elegible for voting");
+        }
+        age=21;
+        if(age<=21){
+            System.out.println("you are elegible for voting");
+        }
+        else{
+            System.out.println("you are not elegible for voting");
+
+        }
+        age=21;
+        if(age<21){
+            System.out.println("you are elegible for voting");
+        }
+        else{
+            System.out.println("you are not elegible for voting");
+
+        }
+    }
+}
