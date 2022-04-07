bcacc904f0b13094af20e22da2a5037eec92a009
abhay <abhay94kushwah>
2022-04-07 12:14:07 +0530

    variables manipulation

---
+++ src/variable_manipulation/Demo3.java
@@ -0,0 +1,24 @@
+package variable_manipulation;
+
+public class Demo3 {
+    public static void main(String[] args){
+        double a=4.5;
+        double b=6.5;
+
+        double sum=a+b;
+
+        System.out.println("sum of a and b" +sum);
+
+        double sub=a-b;
+
+        System.out.println("sub of a and b"+sub);
+
+        double multi=a*b;
+
+        System.out.println("multi of a and b"+multi);
+
+        double divide=a/b;
+
+        System.out.println("divide of a and b"+divide);
+    }
+}
