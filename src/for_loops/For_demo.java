50c73291f7dbbb69adf25c71e7f8850b731d8e94
abhay <abhay94kushwah>
2022-04-10 10:07:40 +0530

    for loops

---
+++ src/for_loops/For_demo.java
@@ -0,0 +1,10 @@
+package for_loops;
+
+public class For_demo {
+    public static void main(String[] args){
+        int five=5;
+        for(int i=1; i<=10; i++){
+            System.out.println("table of 5 :"+(five*i));
+        }
+    }
+}
