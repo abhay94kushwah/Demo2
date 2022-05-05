74d98dcea8e7f5ccd93fffa1fb11eb893e750b4e
abhay <abhay94kushwah>
2022-05-05 15:56:10 +0530

    collection HashMap day 5(for loop)

--- src/hashmap/Hashmap_demo.java
+++ src/hashmap/Hashmap_demo.java
@@ -12,5 +12,25 @@ public class Hashmap_demo {
         System.out.println("printing key :"+map.get("A"));
         System.out.println("printing key :"+map.get("C"));
         System.out.println("printing key :"+map.get("B"));
+
+        for(String var : map.keySet()){
+            System.out.println("printing with keyset :"+map.get(var));
+        }
+
+        HashMap<Integer,String> map1=new HashMap<>();
+
+        map1.put(31,"maths");
+        map1.put(56,"English");
+        map1.put(74,"Chemistry");
+        map1.put(54,"Hindi");
+
+        System.out.println("printing key goal :"+map1.get(31));
+        System.out.println("printing key goal :"+map1.get(56));
+        System.out.println("printing key goal :"+map1.get(74));
+        System.out.println("printing key goal :"+map1.get(54));
+
+        for(Integer var : map1.keySet()){
+            System.out.println("print with keyset :"+map1.get(var));
+        }
     }
 }
