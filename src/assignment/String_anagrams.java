7ef3f280e7b029f218b1d3a929a0f548c4fb9404
Abhay Singh <abhay94kushwah@gmail.com>
2022-08-16 13:08:14 +0530

    assignment 3

--- src/assignment/String_anagrams.java
+++ src/assignment/String_anagrams.java
@@ -1,11 +1,17 @@
 package assignment;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class String_anagrams {
     public static void main(String[] args){
-        String a="listen";
-        String b="silent";
+        String a;
+        String b;
+        System.out.println("Enter Strings");
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        Scanner sc1=new Scanner(System.in);
+        b=sc1.nextLine();
 
         char c[]=a.toCharArray();
         char d[]=b.toCharArray();
