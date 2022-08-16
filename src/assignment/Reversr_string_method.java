ec074643bb6ee92beea58c9aceb9796de356e3d3
Abhay Singh <abhay94kushwah@gmail.com>
2022-08-16 12:39:21 +0530

    assignment1

---
+++ src/assignment/Reversr_string_method.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class Reversr_string_method {
+    public static void main(String[] args){
+        StringBuffer a=new StringBuffer("java");
+        System.out.println(a.reverse());
+
+        StringBuilder b=new StringBuilder("java 8");
+        System.out.println(b.reverse());
+    }
+}
