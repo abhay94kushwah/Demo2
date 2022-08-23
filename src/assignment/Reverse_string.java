335c1e9e5121c468fda5f3bba522e3ff2c9a8462
Abhay Singh <abhay94kushwah@gmail.com>
2022-08-23 10:55:03 +0530

    assignment 5(1)

--- src/assignment/Reverse_string.java
+++ src/assignment/Reverse_string.java
@@ -11,5 +11,16 @@ public class Reverse_string {
 
             System.out.println("Reverse string :" +B[i]);
         }
+
+        String C="DONKEY";
+        String D="";
+
+        for(int i=C.length()-1;i>=0;i--){
+
+            // returns the character at the specified index in a string
+            D=D+C.charAt(i);
+
+            System.out.println("Reversed string :"+D);
+        }
     }
 }
