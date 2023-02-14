c63e96325a5f3670c8df0cbe6e8946afc0b38114
Abhay Singh <abhay94kushwah@gmail.com>
2023-02-14 13:22:33 +0530

    add

---
+++ src/assignment/AddnotillsingleDigit.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class AddnotillsingleDigit {
+    public static void main(String[] args) {
+        int n=12345;
+        int r,sum = 0;
+        while (n>9){
+            sum=0;
+            while (n>0){
+                r=n%10;
+                n=n/10;
+                sum=sum+r;
+            }
+            n=sum;
+        }
+        System.out.println("print single digit :"+sum);
+    }
+}
